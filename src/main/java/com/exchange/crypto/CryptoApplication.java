a9a346290f832b14cf75250fef8aa0218b1c7778
ABERTELLO <bertelloagustin@gmail.com>
2023-05-15 11:03:37 -0300

    new Java project for cryptocurrency tracking

---
+++ src/main/java/com/exchange/crypto/CryptoApplication.java
@@ -0,0 +1,15 @@
+package com.exchange.crypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoApplication.class, args);
+		String saludo = "Hola mundo";
+		System.out.println(saludo);
+	}
+
+}
